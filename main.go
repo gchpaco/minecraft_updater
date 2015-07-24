6349cb8fe0e0684c79bb9c623cc801a212277db9
Graham Hughes <ghughes@meteor.com>
2015-07-24 15:28:06 -0700

    CSV in/out

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
+	"flag"
 	"github.com/moovweb/gokogiri"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
-	"text/tabwriter"
+	"sync"
 )
 
-type Release struct {
-	maturity string
-	filename string
+var sem = make(chan int, 10)
+
+type Mod struct {
+	name, minecraftVersion string
+	currentlyInstalled     string
+	curseForgeURL          string
 }
 
-func releasesFor(url string) (releases []Release, err error) {
+type Release struct {
+	mod                                               *Mod
+	maturity, filename, downloadUrl, minecraftVersion string
+}
+
+func releasesFor(mod *Mod) (releases []*Release, err error) {
+	trueUrl, e := url.Parse(mod.curseForgeURL)
+	if e != nil {
+		err = e
+		return
+	}
 	// fetch and read a web page
-	resp, e := http.Get(url)
+	resp, e := http.Get(mod.curseForgeURL)
 	if e != nil {
 		err = e
 		return
@@ -44,7 +60,8 @@ func releasesFor(url string) (releases []Release, err error) {
 	}
 
 	for _, file := range files {
-		release := &Release{}
+		var release Release
+		release.mod = mod
 		releaseTypes, e := file.Search("./td[contains(@class,\"project-file-release-type\")]/div/@title")
 		if e != nil {
 			err = e
@@ -63,36 +80,105 @@ func releasesFor(url string) (releases []Release, err error) {
 			release.filename = filename.String()
 			break
 		}
-		releases = append(releases, *release)
+		downloadUrls, e := file.Search("./td[contains(@class,\"project-file-name\")]//a[contains(@class,\"overflow-tip\")]/@href")
+		if e != nil {
+			err = e
+			return
+		}
+		for _, downloadUrl := range downloadUrls {
+			localUrl, e := url.Parse(downloadUrl.String())
+			if e != nil {
+				err = e
+				return
+			}
+			release.downloadUrl = trueUrl.ResolveReference(localUrl).String()
+			break
+		}
+		versions, e := file.Search("./td[contains(@class,\"project-file-game-version\")]//span[contains(@class,\"version-label\")]/text()")
+		if e != nil {
+			err = e
+			return
+		}
+		for _, version := range versions {
+			release.minecraftVersion = version.String()
+			break
+		}
+		releases = append(releases, &release)
 	}
 	return
 }
-func latestFor(url string) (release Release, err error) {
-	releases, e := releasesFor(url)
+func reportOn(mod *Mod, out chan *Release) (err error) {
+	sem <- 1
+	releases, e := releasesFor(mod)
 	if e != nil {
 		err = e
 		return
 	}
+	for _, r := range releases {
+		if r.minecraftVersion != mod.minecraftVersion {
+			continue
+		}
+		if r.filename == mod.currentlyInstalled {
+			break
+		}
 
-	release = releases[0]
-	return
-}
-func reportOn(mod, url string, w io.Writer) (err error) {
-	release, e := latestFor(url)
-	if e != nil {
-		err = e
-		return
+		out <- r
 	}
-
-	fmt.Fprintln(w, mod, "\t", release.maturity, "\t", release.filename)
+	<-sem
 	return
 }
 
 func main() {
-	w := tabwriter.NewWriter(os.Stdout, 5, 8, 1, ' ', tabwriter.TabIndent)
-	err := reportOn("Compacter", "http://minecraft.curseforge.com/mc-mods/231549-compacter/files", w)
-	if err != nil {
-		panic(err)
+	flag.Parse()
+	var input io.Reader
+	if flag.NArg() > 1 {
+		log.Fatal("Too many arguments")
+	} else if flag.NArg() == 1 {
+		var err error
+		input, err = os.Open(flag.Arg(0))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		input = os.Stdin
 	}
-	w.Flush()
+	reader := csv.NewReader(input)
+	writer := csv.NewWriter(os.Stdout)
+	reader.FieldsPerRecord = 4
+	updates := make(chan *Release)
+	var wg sync.WaitGroup
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		} else {
+			wg.Add(1)
+			mod := &Mod{name: record[0], minecraftVersion: record[1], currentlyInstalled: record[2], curseForgeURL: record[3]}
+			go func() {
+				defer wg.Done()
+				err := reportOn(mod, updates)
+				if err != nil {
+					log.Println(err)
+				}
+			}()
+		}
+	}
+	go func() {
+		wg.Wait()
+		close(updates)
+	}()
+	for {
+		version, ok := <-updates
+		if (!ok) {
+			break
+		}
+		tuple := [...]string{version.mod.name, version.minecraftVersion, version.filename, version.mod.curseForgeURL, version.downloadUrl, version.maturity}
+		err := writer.Write(tuple[:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		writer.Flush()
+	}
 }
