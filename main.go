c95fcfa852f143ac0c634bbe989b46025b66e36a
Graham Hughes <ghughes@meteor.com>
2015-07-23 21:16:36 -0700

    Factor things out a bit better

--- main.go
+++ main.go
@@ -1,43 +1,98 @@
 package main
 
 import (
+	"fmt"
 	"github.com/moovweb/gokogiri"
+	"io"
 	"io/ioutil"
 	"net/http"
-	"fmt"
 	"os"
 	"text/tabwriter"
 )
 
-func main() {
+type Release struct {
+	maturity string
+	filename string
+}
+
+func releasesFor(url string) (releases []Release, err error) {
 	// fetch and read a web page
-	resp, _ := http.Get("http://minecraft.curseforge.com/mc-mods/231549-compacter/files")
-	page, _ := ioutil.ReadAll(resp.Body)
+	resp, e := http.Get(url)
+	if e != nil {
+		err = e
+		return
+	}
+	page, e := ioutil.ReadAll(resp.Body)
+	if e != nil {
+		err = e
+		return
+	}
 
 	// parse the web page
-	doc, _ := gokogiri.ParseHtml(page)
-
-	w := tabwriter.NewWriter(os.Stdout, 5, 1, 1, ' ', 0)
+	doc, e := gokogiri.ParseHtml(page)
+	if e != nil {
+		err = e
+		return
+	}
+	defer doc.Free()
 
 	html := doc.Root().FirstChild()
-	files, _ := html.Search("//tr[contains(@class,\"project-file-list-item\")]")
+	files, e := html.Search("//tr[contains(@class,\"project-file-list-item\")]")
+	if e != nil {
+		err = e
+		return
+	}
 
 	for _, file := range files {
-		releaseTypes, _ := file.Search("./td[contains(@class,\"project-file-release-type\")]/div/@title")
-		fmt.Fprint(w, "Release:")
+		release := &Release{}
+		releaseTypes, e := file.Search("./td[contains(@class,\"project-file-release-type\")]/div/@title")
+		if e != nil {
+			err = e
+			return
+		}
 		for _, releaseType := range releaseTypes {
-			fmt.Fprint(w, "\t", releaseType)
+			release.maturity = releaseType.String()
+			break
 		}
-		filenames, _ := file.Search("./td[contains(@class,\"project-file-name\")]//a[contains(@class,\"overflow-tip\")]/text()")
+		filenames, e := file.Search("./td[contains(@class,\"project-file-name\")]//a[contains(@class,\"overflow-tip\")]/text()")
+		if e != nil {
+			err = e
+			return
+		}
 		for _, filename := range filenames {
-			fmt.Fprint(w, "\t", filename)
+			release.filename = filename.String()
+			break
 		}
-		fmt.Fprintln(w)
+		releases = append(releases, *release)
+	}
+	return
+}
+func latestFor(url string) (release Release, err error) {
+	releases, e := releasesFor(url)
+	if e != nil {
+		err = e
+		return
+	}
+
+	release = releases[0]
+	return
+}
+func reportOn(mod, url string, w io.Writer) (err error) {
+	release, e := latestFor(url)
+	if e != nil {
+		err = e
+		return
+	}
+
+	fmt.Fprintln(w, mod, "\t", release.maturity, "\t", release.filename)
+	return
+}
+
+func main() {
+	w := tabwriter.NewWriter(os.Stdout, 5, 8, 1, ' ', tabwriter.TabIndent)
+	err := reportOn("Compacter", "http://minecraft.curseforge.com/mc-mods/231549-compacter/files", w)
+	if err != nil {
+		panic(err)
 	}
 	w.Flush()
-
-	// perform operations on the parsed page -- consult the tests for examples
-
-	// important -- don't forget to free the resources when you're done!
-	doc.Free()
 }
